1c69fd4d3318bf02066374aeebb5ded21420d12a
lizzy <lizzy@LIZZY>
2022-11-07 10:54:21 -0600

    Add project files.

---
+++ CODE_CHALLENGE1/Reverse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODE_CHALLENGE1
+{
+    // like private-- only accessible within namespace
+    internal class Reverse
+    {
+       public static void rev(Array ray)
+
+        {
+            // start with empty array
+            int[] newray = new int [ ray.Length ];
+            int counter = 0;
+            // take the array passed in
+            for(int i = ray.Length; i < ray.Length; i++)
+            {
+                newray[counter] = ray[i];
+                counter++;
+
+            }
+
+            // loop through array 
+
+
+            // add
+        }
+        rev(new Array[1, 2, 3, 4, 5]);
+    }
+}
